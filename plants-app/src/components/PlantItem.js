199633e37038ee847c195638ff41d654ba61bae4
Anna <filatovaa@yahoo.com>
2020-02-20 21:02:14 -0800

    search functionallity added to plants

--- plants-app/src/components/PlantItem.js
+++ plants-app/src/components/PlantItem.js
@@ -3,18 +3,19 @@ import React from 'react'
 function PlantItem(props) {
     return (
         <div className = 'plant'>
-            <img src={props.plant.img} alt={props.plant.name}/>
-            <p>Common Name: {props.plant.name}</p>
-            <p>Scientifuc Name: {props.plant.scientificName}</p>
-            <p>Height: {props.plant.height}</p>
-            <p>Temperature: {props.plant.temperature}</p>
-            <p>Humidity: {props.plant.humidity}</p>
-            <ul> Common Issues:
+            <img className='cards' src={props.plant.img} alt={props.plant.name}/>
+            <p><b>Common Name: </b>{props.plant.name}</p>
+            <p><b>Scientific Name: </b> <i>{props.plant.scientificName}</i></p>
+            <p><b>Height: </b>{props.plant.height}</p>
+            <p><b>Temperature: </b>{props.plant.temperature}</p>
+            <p><b>Humidity: </b>{props.plant.humidity}</p>
+            <p> <b>Bugs: </b>{props.plant.bugs}</p>
+            <ul><b>Common Issues:</b>
                 {props.plant.issues.map(item => (
-                    <li key={item}>{item}</li>
+                    <li key={item}> <img src='https://i.imgur.com/vqgeRl4.png?2'/> {item}</li>
                 ))}
             </ul>
-            <p>Pest Problems: {props.plant.bugs}</p>
+            
            
             
             
--- plants-app/src/components/Plants.js
+++ plants-app/src/components/Plants.js
@@ -1,20 +1,55 @@
 import React from 'react'
 import plantList from './plantList'
-import PlantItem from './PlantItem'
 import './Plants.css'
 class Plants extends React.Component {
     constructor(){
         super()
         this.state = {
+            filter:'',
             plants : plantList
         }
+        this.handleChange = this.handleChange.bind(this)
     }
+    handleChange = event => {
+        this.setState({ filter: event.target.value });
+      };
+    
     render() {
-        const plant = this.state.plants.map(plant => < PlantItem key={plant.id} plant = {plant} />)
+            const { filter, plants } = this.state;
+            const lowercasedFilter = filter.toLowerCase();
+            const filteredData = plants.filter(item => {
+              return Object.keys(item).some(key =>
+                typeof item[key]=== 'string' && item[key].toLowerCase().includes(lowercasedFilter)
+              );
+            });
         return (
-            <div className = 'allPlants'>
-                {plant}
+            <div>
+                <header></header>
+                <div>
+                    <input className = 'searchBar' placeholder='search' value={filter} onChange={this.handleChange} />
+                    <div className = 'allPlants'>
+                        {filteredData.map(item => (
+                            <div className = 'plant' key={item.id}>
+                                {/* <div className = 'plant'> */}
+                                    <img className='cards' src={item.img} alt={item.name}/>
+                                        <p><b>Common Name: </b>{item.name}</p>
+                                        <p><b>Scientific Name: </b> <i>{item.scientificName}</i></p>
+                                        <p><b>Height: </b>{item.height}</p>
+                                        <p><b>Temperature: </b>{item.temperature}</p>
+                                        <p><b>Humidity: </b>{item.humidity}</p>
+                                        <p> <b>Bugs: </b>{item.bugs}</p>
+                                        <ul><b>Common Issues:</b>
+                                            {item.issues.map(item => (
+                                                <li key={item}> <img src='https://i.imgur.com/vqgeRl4.png?2'/> {item}</li>
+                                        ))}
+                                         </ul>
+                                </div>
+                            //  </div>
+                        ))}
+                 </div>
             </div>
+            </div>
+         
         )
     }
 
