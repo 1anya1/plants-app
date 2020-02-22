a31e4f874e331b8a1b23232b999c46b8e85387df
Anna <filatovaa@yahoo.com>
2020-02-22 15:17:24 -0800

    working on home page

--- plants-app/src/components/Disease.js
+++ plants-app/src/components/Disease.js
@@ -28,16 +28,18 @@ class Disease extends React.Component {
                 <div className='intro'>
                     <p>Houseplants can be troubled by small, unwelcomed guests. Here is a quick reference guide to help you to identify the signs of pest infestation and how to combat them.</p>
                     <p>The best way to avoid pests is to keep your plant healthy. Healthy plants will be less likely to to fall prey to incects than stressed and unhalthy plants.</p>
-                    <h3 className ='quote'> "Healthy plants will be less likely to to fall prey to incects than stressed and unhalthy plants."</h3>
+                    <h3 className ='quote' id='purpleFont'> "Healthy plants will be less likely to to fall prey to incects than stressed and unhalthy plants."</h3>
                     <p>In addition these pests can also make their way into your home on new plants, so it is very important to inspect new plants prior to bringing them home</p>
                     <p>You are most likely to find these pests on buds, stems, leaves, and the soil of your plant</p>
                     </div>
-                <div className = 'allPests'>
+
+                <h2 className= 'postHeader' id='purpleFontBackground'>Common House Plant Pests</h2>
+                <div className = 'allPests' id='purple'>
                     {this.state.disease.map( disease =>{
                         return (
                             <div className = 'pest' key={disease.id} >
-                                <img className='cards' src={disease.img} alt={disease.name}/>
-                                <p className = 'title'>{disease.name}</p>
+                                <img className='cards' id='purpleBorder' src={disease.img} alt={disease.name}/>
+                                <h1 className = 'title'id='purpleFont' >{disease.name}</h1>
                                 <p><b>Identification:</b> {disease.findThem}</p>
                                 <p><b>Treatment:</b> {disease.treatment}</p>
 
--- plants-app/src/components/Home.js
+++ plants-app/src/components/Home.js
@@ -6,12 +6,19 @@ class Home extends React.Component{
     render() {
         return (
             <div>
-                <header>
-                <h1>Plantly</h1>
-                <h2>All about house plants</h2>
-                <p>Learn how to take care of the common house plants. Look up care instructions, learn about disease and keep track of your plants under one app</p>
-
+                 <header className='plantsCover' id="home">
+                    <div className='header'>
+                        <h1 class='header'>HOUSE  </h1> 
+                        <h1 class='header'><i>PLANTS</i> </h1> 
+                   </div>
                 </header>
+                <div className='intro'>
+                    <p>Houseplants can be troubled by small, unwelcomed guests. Here is a quick reference guide to help you to identify the signs of pest infestation and how to combat them.</p>
+                    <p>The best way to avoid pests is to keep your plant healthy. Healthy plants will be less likely to to fall prey to incects than stressed and unhalthy plants.</p>
+                    <h3 className ='quote' id='purpleFont'> "Healthy plants will be less likely to to fall prey to incects than stressed and unhalthy plants."</h3>
+                    <p>In addition these pests can also make their way into your home on new plants, so it is very important to inspect new plants prior to bringing them home</p>
+                    <p>You are most likely to find these pests on buds, stems, leaves, and the soil of your plant</p>
+                </div>
                
                 {/* <img src= 'https://cdn.vox-cdn.com/thumbor/Q5pJ4OuZrZGmXaakm44sZwTo1Cs=/0x0:4855x3237/1200x900/filters:focal(2046x2422:2822x3198)/cdn.vox-cdn.com/uploads/chorus_image/image/63279027/shutterstock_1140250127.0.jpg' alt='houseplants'></img> */}
 
