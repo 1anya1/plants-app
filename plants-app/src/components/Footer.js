6e1c3843610fd8f457f650761473e2fa1bff9b63
Anna <filatovaa@yahoo.com>
2020-02-29 05:40:44 -0800

    footer completed'

--- plants-app/src/components/Footer.js
+++ plants-app/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Footer extends React.Component{
     render() {
         return (
-            <h1>this is my footer </h1> 
+            <h2 className='footer'>Designed & Built by Anna Filatova</h2>
         )
     }
     
--- plants-app/src/components/Home.js
+++ plants-app/src/components/Home.js
@@ -23,28 +23,23 @@ class Home extends React.Component{
                          </div>
 
                     <p><b style={{color: '#0bbd86', fontSize: 30}}>Keeping</b> plants around the house isn’t as scary as you might think; you 
-                    just need to know what they like in order to keep your indoor garden both happy and green. Plantly is a house plant app that has it all. 
-                    Search common house plants, learn about plant care and gain understanding on how to identify deisease and common pests.</p>
+                    just need to know what they like in order to keep your indoor garden both happy and green. </p>
                         <div><img  className= "banner" src="https://i.imgur.com/ZVbCAnD.png"></img></div>
+                    <p><b style={{color: '#0bbd86', fontSize: 30}}>Plantly</b> is a house plant app that has it all. 
+                    Search common house plants, learn about plant care and gain understanding on how to identify deisease and common pests.</p>
                         <button className = 'home-button'><Link to="/plants" className='buttonGreen'>Discover House Plants</Link></button>
                     
-                    <p><b style={{color: '#0bbd86', fontSize: 30}}>Keep</b> track of your plants using our tracker. Add dates, notes, and photos that you can always refer back to. </p>
-                    <p>You are most likely to find these pests on buds, stems, leaves, and the soil of your plant</p>
-                    <div><img  className= "banner" src="https://i.imgur.com/bjhSHYI.png"></img></div>
-                    <button className = 'home-button'><Link to="/pests" className='buttonGreen'>Visit Plant Tracker</Link></button>
-                   
-                    <p>Here you can find your typical house plants and learn how to take care of them. Also you are 
-                    able to check out common disease and pests that can harm your plants. 
-                    </p>
+                    <p><b style={{color: '#0bbd86', fontSize: 30}}>Keep</b> track of your plants using our tracker. Add dates, notes,
+                     and photos that you can always refer back to. </p>
+                     <div><img  className= "banner" src="https://i.imgur.com/bjhSHYI.png"></img></div>
+                    <p><b style={{color: '#0bbd86', fontSize: 30}}>A</b> couple of words on plants... Proper watering and lighting are the most important components of 
+                    indoor plant care. Humidity and temperatures also play a role. The trick is to try to mimic the climate of the place 
+                    that plant came from.</p>
                         <div className='imagesHome'>
                              <div> <img style={{height:50}} src='https://i.imgur.com/TS5uE6r.png'></img></div>
                              <div> <img style={{height:50}} src='https://i.imgur.com/TS5uE6r.png'></img></div>
                              <div> <img style={{height:50}} src='https://i.imgur.com/TS5uE6r.png'></img></div>
                          </div>
-            
-                    <p>Here you can find your typical house plants and learn how to take care of them. Also you are 
-                    able to check out common disease and pests that can harm your plants. 
-                    </p>
                 </div> 
             </div>
         )
