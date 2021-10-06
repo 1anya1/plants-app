682c4c04e646a2e93c937bfe91a949c3165f86ca
Anna Filatova <filatovaa@yahoo.com>
2021-10-05 17:21:58 -0700

    update to the icons home page

--- plants-app/src/components/Plants/Modal.js
+++ plants-app/src/components/Plants/Modal.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SVG from '../Images.js'
-const nav = document.querySelector('nav')
 export default class Modal extends React.Component {
     constructor(props){
         super(props)
