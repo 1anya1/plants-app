26204704050d28a8b3f7479c20eeefdcc30986d9
Anna Filatova <filatovaa@yahoo.com>
2021-11-04 19:11:35 -0700

    updates to modal in plants

--- plants-app/src/components/Plants/Modal.js
+++ plants-app/src/components/Plants/Modal.js
@@ -20,27 +20,27 @@ export default class Modal extends React.Component {
                         <div className = 'plant-info'>
                         <h4 className='itemName'> {this.props.modal.name}</h4>
                         <div className='plant-spec'>
-                            <div className='subtitle2'>Scientific Name</div>
+                            <div className='overline'>Scientific Name</div>
                             <div className='text italic'>{this.props.modal.scientificName}</div>
                             
                         </div>
                         <div className='plant-spec'>
-                            <div className='subtitle2'>Height</div>
+                            <div className='overline'>Height</div>
                             <div className='text'>{this.props.modal.height}</div>
                         </div>
                         <div className='plant-spec'>
-                            <div className='subtitle2'>Temperature</div>
+                            <div className='overline'>Temperature</div>
                             <div className='text'>{this.props.modal.temperature}</div> 
                         </div>
                         <div className='plant-spec'>
-                            <div className='subtitle2'>Humidity</div>
+                            <div className='overline'>Humidity</div>
                             <div className='text'>{this.props.modal.humidity}</div>
                         </div>
                         <div className='plant-spec'>
-                            <div className='subtitle2'>Pests</div>
+                            <div className='overline'>Pests</div>
                             <div className='text'>{this.props.modal.bugs}</div>
                         </div>
-                        <ul>Common Issues:
+                        <ul className='overline'>Common Issues:
                              {this.props.modal.issues.map(item => (
                                <li className='text' key={item}>{item}</li>
                                    ))}
