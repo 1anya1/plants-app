69c3e4a3f427954af9548888ee3922d6dff77761
Anna Filatova <filatovaa@yahoo.com>
2023-02-09 16:08:56 -0800

    Updated packages and removed unused files, updated backend credentials, updated search bar and improved ui in pages.

--- plants-app/src/components/careTips/CareTips.js
+++ plants-app/src/components/careTips/CareTips.js
@@ -1,65 +1,67 @@
-import React from 'react';
-import mySVG from '../data/Svg'
-import './CareTips.scss'
-import careTips from './careTipsSvg'
+import React from "react";
+import mySVG from "../data/Svg";
+import "./CareTips.scss";
+import careTips from "./careTipsSvg";
 
+const careType = [
+  "Sun",
+  "Watering",
+  "Humidity",
+  "Temperature",
+  "Fertilizing",
+  "Repotting",
+  "Prunning",
+  "TravelCare",
+];
 
-const careType = [ 'Sun', 'Watering', 'Humidity', 'Temperature', 'Fertilizing', 'Repotting', 'Prunning', 'TravelCare'
-    ]
-
-class CareTips extends React.Component{
-    render(){
-       return (
-        <>
-       <div className="hero intro" id='care-tips' >
-            <div className='content'>
-                <h2>Tips to keep your green friends healthy and happy</h2>
-                {/* <div className='subtitle'>The trick is to try to mimic the climate of the place that plant came from..</div> */}
-            </div>
-            <div className='image'></div>
-
-              
+class CareTips extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="hero intro " id="care-tips">
+          <div className="content">
+            <h4>Tips to keep your green friends healthy and happy</h4>
+          </div>
+          <div className="image"></div>
         </div>
-        <div className='care-tips'>
-            <div className='care-list'>
-                {Object.keys(mySVG).map((el,idx)=>{
-                    const Component = mySVG[el]
-                    const Data = careTips[careType[idx]]
-                    return (
-                    <>
-                        <a href={ `#${careType[idx]}`} >
-                            <div className='symbols'>
-                                <div className='care'>
-                                    < Component  />
-                                    <h5>{careType[idx]}</h5>
-                                </div>
-                            </div>
-                        </a>
-                    </>
-                    )
-                })}
-            </div>
-            {careType.map((el, idx)=>{
-                const Data = careTips[el]
-                return(
-                    <div className='paragraphs intro'>
-                        <div className='subtitle2' id={el}>{careType[idx]}</div>
-                        {Data.map((el, idx)=>{
-                            if(idx%2===0){
-                                return  <h4>{el}</h4>
-                            } else{
-                                return  <h5>{el}</h5>
-                            }
-                        })}
+        <div className="care-tips intro">
+          <div className="care-list">
+            {Object.keys(mySVG).map((el, idx) => {
+              return (
+                <a href={`#${careType[idx]}`}>
+                  <div className="symbols">
+                    <div className="care">
+                      {/* < Component  /> */}
+                      <h5>{careType[idx]}</h5>
                     </div>
-                )
-               
+                  </div>
+                </a>
+              );
             })}
+          </div>
+          {careType.map((el, idx) => {
+            const Data = careTips[el];
+            return (
+              <div className="paragraphs ">
+                <div className="header">
+                  <p id={el} className="subtitle2">
+                    {careType[idx]}
+                  </p>
+                </div>
+                {Data.map((el, idx) => {
+                  if (idx % 2 === 0) {
+                    return <h6>{el}</h6>;
+                  } else {
+                    return <p>{el}</p>;
+                  }
+                })}
+              </div>
+            );
+          })}
         </div>
-        </>
-       ) 
-    }
+      </>
+    );
+  }
 }
 
-export default CareTips
-
+export default CareTips;
